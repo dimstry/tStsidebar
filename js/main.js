8d08a7280b00ad63e484a5d37af03454f3a141b4
dimstry <dimastriana03@gmail.com>
2022-08-11 18:31:26 +0700

    finish massage

--- js/main.js
+++ js/main.js
@@ -4,25 +4,32 @@ const navSection = document.querySelector(".nav_section");
 const headerLeft = document.querySelector(".header_left");
 const headerRight = document.querySelector(".header_right");
 const navInfo = document.querySelectorAll(".nav_info")
-const menu = document.querySelector(".menu");
+const menu = document.querySelectorAll(".head_text");
 const menuItem = document.querySelectorAll(".menu_item");
 const menuLink = document.querySelectorAll(".menu_link")
 const notif = document.querySelector(".notif");
+const person = document.querySelectorAll(".person");
+const massageNotif = document.querySelectorAll(".notif_massage");
+const massage = document.querySelectorAll(".massage");
+const tool = document.querySelector(".tool")
 
 // function display & text
 const forDisplay = (text, display) => {
     headerRight.style.display = display;
     notif.style.display = display;
-    menu.style.textAlign = text;
+    tool.style.display = display;
 }
 
 // function loop menu
-const forLoop = (display, classL) => {
+const forLoop = (display, classL, text) => {
     for (let i = 0; i < navInfo.length; i++) {
         navInfo[i].style.display = display;
     }
     for (let i = 0; i < menuItem.length; i++) {
         menuItem[i].classList.add(classL);
+    }
+    for (let i = 0; i < menu.length; i++) {
+        menu[i].style.textAlign =text;
     }
 }
 
@@ -37,8 +44,17 @@ const forWidth = (section, head) => {
 roundBtn.addEventListener('click', () => {
     console.log('tombol di klik mang');
     forWidth("5.5rem","100%");
-    forLoop("none", "respon");
+    forLoop("none", "respon", "center");
     forDisplay("center", "none");
+    for (let i = 0; i < person.length; i++) {
+        person[i].style.display = "none";
+    }
+    for (let i = 0; i < massageNotif.length; i++) {
+        massageNotif[i].style.display = "none";
+    }
+    for (let i = 0; i < massage.length; i++) {
+        massage[i].style.justifyContent = "center";
+    }
 })
 
 
